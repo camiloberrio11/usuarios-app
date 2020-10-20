bf6dffeba4a9df857777e2e0b0966b2845f30ddf
jberrio <jberrio@transfiriendo.com>
2020-10-20 06:52:44 -0500

    Se inicia con componente de header y componente de boton de obtencion de usuario

---
+++ src/components/ButtonGetUsuario.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ButtonGetUsuario = () => {
+  return (
+    <div className="container btn-action">
+    <button className="btn btn-danger nuevo-post d-block d-md-inline-block mt-8">Obtener usuarios</button>
+    <hr />
+    </div>
+  )
+}
+
+export default ButtonGetUsuario
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
+      <div className="container">
+        <h1> <Link to={'/'} className="text-light"> Usuarios App </Link></h1>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
